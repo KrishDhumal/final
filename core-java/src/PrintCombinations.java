46b105a4a5e11af44e38d16ce8c1897262267e8d
krishdhumal <krishdhumal@localhost>
2025-04-14 21:28:08 +0530

    second

--- core-java/src/PrintCombinations.java
+++ core-java/src/PrintCombinations.java
@@ -1,11 +1,31 @@
-
 public class PrintCombinations {
 
-	public static void main(String[] args) {
-		
-		String str = "abc";
-		String k = "k;"
-		
-		//hiii
-	}
+    public static void main(String[] args) {
+        String str = "abc";
+
+        for (int length = 1; length <= 3; length++) 
+        {
+            for (int i = 0; i < str.length(); i++) 
+            {
+                if (length == 1) 
+                {
+                    System.out.println(str.charAt(i));
+                }
+                for (int j = 0; j < str.length(); j++) 
+                {
+                    if (length == 2 && i != j) 
+                    {
+                        System.out.println("" + str.charAt(i) + str.charAt(j));
+                    }
+                    for (int k = 0; k < str.length(); k++) 
+                    {
+                        if (length == 3 && i != j && j != k && i != k) 
+                        {
+                            System.out.println("" + str.charAt(i) + str.charAt(j) + str.charAt(k));
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
