df9dc232b817dc109842014def79ba356be7656c
krishdhumal <krishdhumal@localhost>
2025-04-14 21:31:21 +0530

    k

--- core-java/src/PrintCombinations.java
+++ core-java/src/PrintCombinations.java
@@ -3,23 +3,23 @@ public class PrintCombinations {
     public static void main(String[] args) {
         String str = "abc";
 
-        for (int length = 1; length <= 3; length++) 
+        for (int n = 1; n <= 3; n++) 
         {
             for (int i = 0; i < str.length(); i++) 
             {
-                if (length == 1) 
+                if (n == 1) 
                 {
                     System.out.println(str.charAt(i));
                 }
                 for (int j = 0; j < str.length(); j++) 
                 {
-                    if (length == 2 && i != j) 
+                    if (n == 2 && i != j) 
                     {
                         System.out.println("" + str.charAt(i) + str.charAt(j));
                     }
                     for (int k = 0; k < str.length(); k++) 
                     {
-                        if (length == 3 && i != j && j != k && i != k) 
+                        if (n == 3 && i != j && j != k && i != k) 
                         {
                             System.out.println("" + str.charAt(i) + str.charAt(j) + str.charAt(k));
                         }
